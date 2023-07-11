7a94c74f4dcd0b8903288a97d64225ed2d2942c9
nguyencongthang2509 <congthang25092003@gmail.com>
2023-07-11 22:07:31 +0700

    update remove userdetails

--- server/src/main/java/com/example/user_management/infrastructure/security/WebSecurityConfiguration.java
+++ server/src/main/java/com/example/user_management/infrastructure/security/WebSecurityConfiguration.java
@@ -5,13 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -57,19 +54,6 @@ public class WebSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new CustomUserDetailsService();
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService());
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        return authenticationProvider;
     }
 
 }
--- server/src/main/java/com/example/user_management/service/impl/AuthServiceImpl.java
+++ server/src/main/java/com/example/user_management/service/impl/AuthServiceImpl.java
@@ -3,7 +3,6 @@ package com.example.user_management.service.impl;
 import com.example.user_management.entity.User;
 import com.example.user_management.infrastructure.constant.Message;
 import com.example.user_management.infrastructure.exception.rest.RestApiException;
-import com.example.user_management.infrastructure.security.CustomUserDetailsService;
 import com.example.user_management.infrastructure.security.JwtTokenProvider;
 import com.example.user_management.model.request.LoginRequest;
 import com.example.user_management.model.response.JwtResponse;
@@ -11,7 +10,6 @@ import com.example.user_management.repository.UserRepository;
 import com.example.user_management.repository.UserTokenRepository;
 import com.example.user_management.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
@@ -31,19 +29,15 @@ public class AuthServiceImpl implements AuthService {
     private UserTokenRepository userTokenRepository;
 
     @Autowired
-    private CustomUserDetailsService customUserDetailsService;
-
-    @Autowired
     private PasswordEncoder passwordEncoder;
 
     @Override
     public JwtResponse loginGoogle(String tokenId) {
         User userVerify = GoogleTokenVerifier.verifyToken(tokenId);
-        UserDetails userDetails = customUserDetailsService.loadUserByUsername(userVerify.getEmail());
-        if (userDetails == null) {
+        User userFind = userRepository.findUserByEmail(userVerify.getEmail());
+        if (userFind == null) {
             throw new RestApiException(Message.USER_NOT_EXISTS);
         }
-        User userFind = userRepository.findUserByEmail(userVerify.getEmail());
         String jwtToken = jwtTokenProvider.generateTokenUser(userFind);
         JwtResponse jwtResponse = new JwtResponse();
         jwtResponse.setToken(jwtToken);
@@ -52,11 +46,10 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public JwtResponse loginBasic(@RequestBody LoginRequest loginRequest) {
-        UserDetails userDetails = customUserDetailsService.loadUserByUsername(loginRequest.getEmail());
-        if (!passwordEncoder.matches(loginRequest.getPassword(), userDetails.getPassword())) {
+        User userFind = userRepository.findUserByEmail(loginRequest.getEmail());
+        if (!passwordEncoder.matches(loginRequest.getPassword(), userFind.getPassword())) {
             throw new RestApiException(Message.EMAIL_OR_PASSWORD_INCORRECT);
         }
-        User userFind = userRepository.findUserByEmail(loginRequest.getEmail());
         String jwtToken = jwtTokenProvider.generateTokenUser(userFind);
         JwtResponse jwtResponse = new JwtResponse();
         jwtResponse.setToken(jwtToken);
